139ee40ea8aed2082add1043812f376510281679
Matthew Lundeen <97109488+decentishDev@users.noreply.github.com>
2023-01-18 13:20:05 -0600

    fixing (not really)

--- js/script.js
+++ js/script.js
@@ -27,28 +27,7 @@ function drawRectangles() {
         horizontalLines.push(true);
         verticalLines.push(true);
     }
-    var completelyDone = false;
-    while(!completelyDone){
-        while(!horizontalDone || !verticalDone){
-            deleteOne();
-        }
-        var done = true;
-        for(let i = 0; i<squareGroups.length; i++){
-            if(squareGroups[i] != "0a"){
-                done = false;
-                break
-            }
-        }
-        if(done){
-            completelyDone = true;
-        }else{
-            horizontalDone = false;
-            verticalDone = false;
-        }
-    }
     
-
-    console.log(squareGroups);
 
     for(let y = 0; y<size-1; y++){
         for(let x = 0; x<size; x++){
@@ -65,108 +44,179 @@ function drawRectangles() {
         }
     }
 }
-
 function deleteOne(){
-    var sidePicked = Math.floor(Math.random() * 2);
+    var sidePicked = Random.Range(0, 2);
     if(horizontalDone){
         sidePicked = 1;
     }
     if(verticalDone){
         sidePicked = 0;
     }
-    if(horizontalDone && verticalDone){
+    if(horizontalDone & verticalDone){
         sidePicked = 2;
-        console.log(squareGroups);
     }
     if(sidePicked == 0){
-        var currentIndexes = [];
-        for (let i = 0; i < horizontalLines.length; i++){
-            if(horizontalLines[i]){
-                currentIndexes.push(i);
+        List<int> currentIndexes = new List<int>();
+        for(var x = 0; x<horizontalLines.Count; x++){
+            if(horizontalLines[x] == true){
+                currentIndexes.Add(x);
             }
         }
-        var currentChoice = currentIndexes[Math.floor(Math.random() * currentIndexes.length)];
-        for(let i = 0; i < currentIndexes.length; i++){
+        var index = Random.Range(0, currentIndexes.Count);
+        var currentChoice = currentIndexes[index];
+        var indexesLength = currentIndexes.Count;
+        for(var i = 0; i<indexesLength; i++){
             var thisRow = 0;
             var thisColumn = 0;
-            for(let x = 0; x < size; x++){
-                if((currentChoice) - (size*(x + 1)) < 0){
+            for(var x = 0; x < sizeY; x++){
+                if((currentChoice) - (sizeX*(x + 1)) < 0){
                     thisRow = x;
-                    thisColumn = currentChoice - size*x;
+                    thisColumn = currentChoice - sizeX*x;
                     break;
                 }
             }
-            if(squareGroups[(thisRow*size) + thisColumn] == squareGroups[((thisRow+1)*size) + thisColumn]){
-                //let arrayLength = currentIndexes.length
-                let index = currentIndexes.indexOf(currentChoice);
-                let testsidk = currentIndexes.splice(index-1);
-                //console.log(String(currentIndexes.length) + " " + String(arrayLength));
-                if(currentIndexes.length == 0){
+            //Debug.Log(thisRow.ToString() + "  " + thisColumn.ToString() + "  " + currentChoice.ToString());
+            if(squareGroups[(thisRow*sizeX) + thisColumn] == squareGroups[((thisRow+1)*sizeX) + thisColumn]){
+                //Debug.Log("horizontal same");
+                //Debug.Log("Horizontal before " +string.Join(" ", currentIndexes.ToArray()));
+                currentIndexes.Remove(currentChoice);
+                //Debug.Log("Horizontal after " +string.Join(" ", currentIndexes.ToArray()));
+                //Debug.Log(currentIndexes.Count);
+                if(currentIndexes.Count == 0){
                     horizontalDone = true;
                     break;
                 }else{
-                    currentChoice = currentIndexes[Math.floor(Math.random() * currentIndexes.length)];
+                    var index1 = Random.Range(0, currentIndexes.Count);
+                    currentChoice = currentIndexes[index1];
                 }
             }else{
-                //onsole.log("horizontal" + String(currentChoice) + " " + String((thisRow*size) + thisColumn) + " " + String(((thisRow+1)*size) + thisColumn));
+                //Debug.Log("horizontal different");
+                //print("horizontal" + str(currentChoice) + " " + str(squareGroups[(thisRow*size) + thisColumn]) + " " + str(squareGroups[((thisRow+1)*size) + thisColumn]))
                 horizontalLines[currentChoice] = false;
-                var changingIndexes = [];
-                for(let x = 0; x<squareGroups.length; x++){
-                    if(squareGroups[x] == squareGroups[((thisRow+1)*size) + thisColumn]){
-                        changingIndexes.push(x);
+                List<int> changingIndexes = new List<int>();
+                for(var x = 0; x<squareGroups.Count; x++){
+                    if(squareGroups[x] == squareGroups[((thisRow+1)*sizeX) + thisColumn]){
+                        changingIndexes.Add(x);
                     }
                 }
-                for(let x = 0; x<changingIndexes.length; x++){
-                    squareGroups[changingIndexes[x]] = squareGroups[(thisRow*size) + thisColumn];
+                // = squareGroups[(thisRow*size) + thisColumn]
+                for(var x = 0; x<changingIndexes.Count; x++){
+                    squareGroups[changingIndexes[x]] = squareGroups[(thisRow*sizeX) + thisColumn];
                 }
                 break;
             }
         }
     }else if(sidePicked == 1){
-        var currentIndexes = [];
-        for (let i = 0; i < verticalLines.length; i++){
-            if(verticalLines[i]){
-                currentIndexes.push(i);
+        List<int> currentIndexes = new List<int>();
+        for(var x = 0; x<verticalLines.Count; x++){
+            if(verticalLines[x] == true){
+                currentIndexes.Add(x);
             }
         }
-        var currentChoice = currentIndexes[Math.floor(Math.random() * currentIndexes.length)];
-        for(let i = 0; i < currentIndexes.length; i++){
+        var index = Random.Range(0, currentIndexes.Count);
+        var currentChoice = currentIndexes[index];
+        var indexesLength = currentIndexes.Count;
+        for(var i = 0; i<indexesLength; i++){
             var thisRow = 0;
             var thisColumn = 0;
-            for(let x = 0; x < size; x++){
-                if((currentChoice) - ((size-1)*(x + 1)) < 0){
+            for(var x = 0; x < sizeY; x++){
+                if((currentChoice) - ((sizeX-1)*(x + 1)) < 0){
                     thisRow = x;
-                    thisColumn = currentChoice - ((size-1)*x);
+                    thisColumn = currentChoice - ((sizeX-1)*x);
                     break;
                 }
             }
-            if(squareGroups[(thisRow*size) + thisColumn] == squareGroups[(thisRow*size) + thisColumn + 1]){
-                //console.log(currentIndexes);
-                let index = currentIndexes.indexOf(currentChoice);
-                let textidk = currentIndexes.splice(index);
-                //console.log(currentIndexes);
-                if(currentIndexes.length == 0){
+            if(squareGroups[(thisRow*sizeX) + thisColumn] == squareGroups[(thisRow*sizeX) + thisColumn + 1]){
+                //Debug.Log("vertical same");
+                //Debug.Log("Vertical before " + string.Join(" ", currentIndexes.ToArray()));
+                currentIndexes.Remove(currentChoice);
+                //Debug.Log("Vertical after " + string.Join(" ", currentIndexes.ToArray()));
+                //Debug.Log(currentIndexes.Count);
+                if(currentIndexes.Count == 0){
                     verticalDone = true;
                     break;
                 }else{
-                    currentChoice = currentIndexes[Math.floor(Math.random() * currentIndexes.length)];
+                    var index1 = Random.Range(0, currentIndexes.Count);
+                    currentChoice = currentIndexes[index1];
                 }
             }else{
-                //console.log("vertical" + String(currentChoice) + " " + String((thisRow*size) + thisColumn) + " " + String((thisRow*size) + thisColumn + 1));
+                //Debug.Log("vertical different");
+                //print("vertical" + str(currentChoice) + " " + str(squareGroups[(thisRow*size) + thisColumn]) + " " + str(squareGroups[(thisRow*size) + thisColumn + 1]))
                 verticalLines[currentChoice] = false;
-                var changingIndexes = [];
-                for(let x = 0; x<squareGroups.length; x++){
-                    if(squareGroups[x] == squareGroups[(thisRow*size) + thisColumn + 1]){
-                        changingIndexes.push(x);
+                List<int> changingIndexes = new List<int>();
+                for(var x = 0; x<squareGroups.Count; x++){
+                    if(squareGroups[x] == squareGroups[(thisRow*sizeX) + thisColumn + 1]){
+                        changingIndexes.Add(x);
                     }
                 }
-                for(let x = 0; x<changingIndexes.length; x++){
-                    squareGroups[changingIndexes[x]] = squareGroups[(thisRow*size) + thisColumn];
+                // = squareGroups[(thisRow*size) + thisColumn]
+                for(var x = 0; x<changingIndexes.Count; x++){
+                    squareGroups[changingIndexes[x]] = squareGroups[(thisRow*sizeX) + thisColumn];
                 }
                 break;
-            }
+            } 
         }
     }
+
+    if(horizontalDone != true || verticalDone != true){
+        deleteOne();
+    }
+    // if(timesLeft>0f){
+    //     timesLeft-=1f;
+    //     deleteOne();
+    // }
+}
+const canvas = document.querySelector('#canvas');
+
+if (!canvas.getContext) {
+    return;
 }
 
-drawRectangles();
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = '#000000';
+
+ctx.fillRect(25, 25, (size - 1) * 25, 2);
+ctx.fillRect(50, (size*25) + 25, (size - 1) * 25, 2);
+ctx.fillRect(25, 25, 2, size*25);
+ctx.fillRect((size*25) + 25, 25, 2, size*25);
+
+// for (let i = 0; i < size*size; i++) {
+//     squareGroups.push(String(i)+"a");
+// }
+// for (let i = 0; i < size*(size-1); i++) {
+//     horizontalLines.push(true);
+//     verticalLines.push(true);
+// }
+
+
+for(var x = 0; x < sizeX*sizeY; x++){
+    squareGroups.Add(x.ToString());
+}
+for(var x = 0; x < sizeX*(sizeY-1); x++){
+    horizontalLines.Add(true);
+}
+for(var x = 0; x < sizeY*(sizeX-1); x++){
+    verticalLines.Add(true);
+}
+
+deleteOne();
+
+while(true){
+    if(horizontalDone && verticalDone){
+        for(let y = 0; y<size-1; y++){
+            for(let x = 0; x<size; x++){
+                if(horizontalLines[y*size + x] == true){
+                    ctx.fillRect((x*25) + 25, (y*25) + 50, 25, 2);
+                }
+            }
+        }
+        for(let y = 0; y<size; y++){
+            for(let x = 0; x<size-1; x++){
+                if(verticalLines[y*(size-1) + x] == true){
+                    ctx.fillRect((x*25) + 50, (y*25) + 25, 2, 25);
+                }
+            }
+        }
+        break;
+    }
+}
