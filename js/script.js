d7d60bd6257d527f4d2cf39ed52081a3f8f3f69c
Matthew Lundeen <97109488+decentishDev@users.noreply.github.com>
2023-01-18 17:17:53 -0600

    fixing canvas

--- js/script.js
+++ js/script.js
@@ -168,9 +168,9 @@ function deleteOne(){
 }
 const canvas = document.querySelector('#canvas');
 
-if (!canvas.getContext) {
-    return;
-}
+// if (!canvas.getContext) {
+//     return;
+// }
 
 const ctx = canvas.getContext('2d');
 ctx.fillStyle = '#000000';
